c7ea5cbafb2401c786621ca00d66c021a4f56474
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-01-14 14:16:08 -0600

    Add pytest for trim

---
+++ workflow/tests/test_trimData.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pytest
+import pandas as pd
+from io import StringIO
+import os
+
+test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
+                '/../../'
+
+@pytest.mark.trimData_se
+def test_trimData_se():
+    assert os.path.exists(os.path.join(test_output_path, '16-1ZX4_trimmed.fq.gz'))
+
+@pytest.mark.trimData_pe
+def test_trimData_pe():
+    assert os.path.exists(os.path.join(test_output_path, 'Q-Y5JA_R1_val_1.fq.gz'))
+    assert os.path.exists(os.path.join(test_output_path, 'Q-Y5JA_R2_val_2.fq.gz'))
