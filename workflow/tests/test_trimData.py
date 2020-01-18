44a7326b15ccf0e101da66e18171cb5c87d5cd01
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-01-18 16:08:43 -0600

    Split unit test for trimData to 2

--- workflow/tests/test_trimData.py
+++ workflow/tests/test_trimData.py
@@ -9,7 +9,9 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
                 '/../../'
 
 @pytest.mark.trimData
-def test_trimData():
+def test_trimData_se():
     assert os.path.exists(os.path.join(test_output_path, '16-1ZX4_trimmed.fq.gz'))
+
+def test_trimData_pe():
     assert os.path.exists(os.path.join(test_output_path, 'Q-Y5JA_R1_val_1.fq.gz'))
     assert os.path.exists(os.path.join(test_output_path, 'Q-Y5JA_R2_val_2.fq.gz'))
