dbdf865f1ea01aa25a233103d4f283329729951f
Venkat Malladi <venkat.malladi@utsouthwestern.edu>
2020-01-28 07:54:37 -0600

    Update read comparison and decorator

--- workflow/tests/test_alignReads.py
+++ workflow/tests/test_alignReads.py
@@ -13,12 +13,13 @@ logs_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 @pytest.mark.alignData
 def test_alignData_se():
 	assert os.path.exists(os.path.join(data_output_path, '16-1ZX4.unal.gz'))
-	assert utils.count_lines(os.path.join(data_output_path, '16-1ZX4.unal.gz')) == 3070504
+	assert utils.count_lines(os.path.join(data_output_path, '16-1ZX4.unal.gz')) == 3070528
 	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.bam'))
 	assert utils.count_lines(os.path.join(data_output_path, 'Q-Y5JA.sorted.bam')) == 5805611
 	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.bai'))
 	assert utils.count_lines(os.path.join(data_output_path, 'Q-Y5JA.sorted.bai')) == 12824
 
+@pytest.mark.alignData
 def test_alignData_pe():
 	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.unal.gz'))
 	assert utils.count_lines(os.path.join(data_output_path, 'Q-Y5JA.unal.gz')) == 0
@@ -36,6 +37,7 @@ def test_alignLogs_se():
 	assert os.path.exists(os.path.join(logs_output_path, '16-1ZX4.align.out'))
 	assert utils.count_lines(os.path.join(logs_output_path, '16-1ZX4.align.out')) == 0
 
+@pytest.mark.alignLogs
 def test_alignLogs_pe():
 	assert os.path.exists(os.path.join(logs_output_path, 'Q-Y5JA.align.err'))
 	assert utils.count_lines(os.path.join(logs_output_path, 'Q-Y5JA.align.err')) == 7
