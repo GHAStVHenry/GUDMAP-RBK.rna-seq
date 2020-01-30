d16e6c862d7e347151c2720b313ae61734884b6b
Jonathan Gesell <jonathan.gesell@utsouthwestern.edu>
2020-01-30 12:07:08 -0600

    Updated output file names for unit test.

--- workflow/tests/test_dedupReads.py
+++ workflow/tests/test_dedupReads.py
@@ -13,13 +13,13 @@ logs_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 
 @pytest.mark.dedupData
 def test_dedupData_se():
-	assert os.path.exists(os.path.join(data_output_path, '16-1ZX4.deduped.bm'))
-	assert os.path.exists(os.path.join(data_output_path, '16-1ZX4.deduped.Metrics.txt'))
+	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.deduped.bam'))
+	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.deduped.Metrics.txt'))
 
 @pytest.mark.dedupData
 def test_dedupData_pe():
-	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.deduped.bm'))
-	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.deduped.Metrics.txt'))
+	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.pe.deduped.bam'))
+	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.pe.deduped.Metrics.txt'))
 
 
 @pytest.mark.dedupLogs
