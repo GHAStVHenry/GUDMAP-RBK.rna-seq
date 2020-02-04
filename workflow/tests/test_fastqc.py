645546fa4b648e281c3c349ae90cb11dc6e1df63
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-02-04 08:27:44 -0600

    Fix fastqc pytest name

--- workflow/tests/test_fastqc.py
+++ workflow/tests/test_fastqc.py
@@ -9,5 +9,5 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
                 '/../../'
 
 @pytest.mark.fastqc
-def test_trimData_se():
+def test_fastqc():
     assert os.path.exists(os.path.join(test_output_path, 'Q-Y5JA_1M.R1_fastqc.zip'))
