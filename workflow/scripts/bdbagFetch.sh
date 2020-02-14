66591ecd4ec0f56b7a315b643c22da19869a655e
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-02-14 13:29:16 -0600

    Fix if not exist in bdbagFetch.sh

--- workflow/scripts/bdbagFetch.sh
+++ workflow/scripts/bdbagFetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z ${3} ]
+if [ -z "${3}" ]
 then
 bdbag --resolve-fetch all --fetch-filter filename\$*fastq.gz ${1}
     for i in $(find */ -name "*.R*.fastq.gz")
@@ -8,7 +8,7 @@ bdbag --resolve-fetch all --fetch-filter filename\$*fastq.gz ${1}
         path=${2}$(echo ${i##*/} | grep -o "\.R.\.fastq\.gz")
         mv ${i} ./${path}
     done
-elif [ ${3} == "TEST" ]
+elif [ "${3}" == "TEST" ]
 then
     bdbag --resolve-fetch all --fetch-filter filename\$*.txt ${1}
 fi
