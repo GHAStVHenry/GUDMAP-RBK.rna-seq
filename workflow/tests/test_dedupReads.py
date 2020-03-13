f50feedde1b9e992f715d696e0343a0b3f720b32
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-03-12 20:28:06 -0500

    Add bai test to dedup unit ci pytest

--- workflow/tests/test_dedupReads.py
+++ workflow/tests/test_dedupReads.py
@@ -12,3 +12,4 @@ data_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 @pytest.mark.dedupData
 def test_dedupData():
 	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.deduped.bam'))
+	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.deduped.bam.bai'))
