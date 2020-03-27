2321e6ccb78c354eafd259ad2ab3e7d0b417d47b
Jonathan Gesell <jonathan.gesell@utsouthwestern.edu>
2020-03-27 09:31:43 -0500

    Update test_dedupReads.py

--- workflow/tests/test_dedupReads.py
+++ workflow/tests/test_dedupReads.py
@@ -11,7 +11,7 @@ data_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 
 @pytest.mark.dedupData
 def test_dedupData():
-	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.sorted.deduped.bam'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.sorted.deduped.bam'))
     assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.sorted.deduped.bam.bai'))
     assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr1.bam'))
     assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr1.bam.bai'))
