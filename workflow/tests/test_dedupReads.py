a06578adf6190d8a4e4440a83f80ee3c1659a849
Jonathan Gesell <jonathan.gesell@utsouthwestern.edu>
2020-03-27 09:17:43 -0500

    Update test_dedupReads.py to include split, testing chr1, chr2, chr8

--- workflow/tests/test_dedupReads.py
+++ workflow/tests/test_dedupReads.py
@@ -13,3 +13,9 @@ data_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 def test_dedupData():
 	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.sorted.deduped.bam'))
 	assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA_1M.se.sorted.deduped.bam.bai'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr1.bam'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr1.bam.bai'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr2.bam'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr2.bam.bai'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr8.bam'))
+    assert os.path.exists(os.path.join(data_output_path, 'Q-Y5JA.sorted.deduped.chr8.bam.bai'))
