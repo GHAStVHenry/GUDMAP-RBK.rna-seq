0c0407f928123f5cd636f51326d4248a5f851706
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-05-04 17:22:10 -0500

    Change fetched fastq's mv to cv... mv was truncating some files

--- workflow/scripts/bdbagFetch.sh
+++ workflow/scripts/bdbagFetch.sh
@@ -2,11 +2,11 @@
 
 if [ -z "${3}" ]
 then
-bdbag --resolve-fetch all --fetch-filter filename\$*fastq.gz ${1}
+    bdbag --resolve-fetch all --fetch-filter filename\$*fastq.gz ${1}
     for i in $(find */ -name "*.R*.fastq.gz")
     do
         path=${2}$(echo ${i##*/} | grep -o "\.R.\.fastq\.gz")
-        mv ${i} ./${path}
+        cp ${i} ./${path}
     done
 elif [ "${3}" == "TEST" ]
 then
