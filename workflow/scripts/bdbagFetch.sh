c02d977abd1a2ca4c4b7d1d5d5f09046a96db5ae
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-05-06 11:10:50 -0500

    Generalize find fastq after fetch

--- workflow/scripts/bdbagFetch.sh
+++ workflow/scripts/bdbagFetch.sh
@@ -3,9 +3,9 @@
 if [ -z "${3}" ]
 then
     bdbag --resolve-fetch all --fetch-filter filename\$*fastq.gz ${1}
-    for i in $(find */ -name "*.R*.fastq.gz")
+    for i in $(find */ -name "*R*.fastq.gz")
     do
-        path=${2}$(echo ${i##*/} | grep -o "\.R.\.fastq\.gz")
+        path=16-TA16$(echo ${i##*/} | grep -o "\.R.\.fastq\.gz")
         cp ${i} ./${path}
     done
 elif [ "${3}" == "TEST" ]
