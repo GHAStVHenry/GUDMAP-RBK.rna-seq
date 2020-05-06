a38391848f045661783a2baa1b033ef0ddefd398
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-05-06 11:58:25 -0500

    Generalize destination filename for fastq qxtraction

--- workflow/scripts/bdbagFetch.sh
+++ workflow/scripts/bdbagFetch.sh
@@ -5,7 +5,7 @@ then
     bdbag --resolve-fetch all --fetch-filter filename\$*fastq.gz ${1}
     for i in $(find */ -name "*R*.fastq.gz")
     do
-        path=16-TA16$(echo ${i##*/} | grep -o "\.R.\.fastq\.gz")
+        path=${2}$(echo ${i##*/} | grep -o "R[1,2]").fastq.gz
         cp ${i} ./${path}
     done
 elif [ "${3}" == "TEST" ]
