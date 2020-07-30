6ed76f56f62c3058a120be0919b7bf9f9e81f44c
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-07-30 12:27:29 -0500

    Add initial trial for dataQC unit improvement #48

--- workflow/tests/test_dataQC.py
+++ workflow/tests/test_dataQC.py
@@ -11,4 +11,14 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 @pytest.mark.dataQC
 def test_dataQC():
     assert os.path.exists(os.path.join(test_output_path, 'Q-Y5F6_1M.se.sorted.deduped.tin.xls'))
+    assert countLines(os.path.join(test_output_path, 'Q-Y5F6_1M.se.sorted.deduped.tin.xls'))
 
+def countLines(fileName):
+    data = False
+    file = open(fileName, "r")
+    for line in file:
+        if file.readlines()[6] != "geneID":
+            data = True
+            break
+
+    return data
