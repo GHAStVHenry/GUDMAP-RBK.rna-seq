df3d4788c4f61dcd3b59b5c68ee2142b8abc4d36
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-12-29 00:33:45 -0600

    Update all tests with new test data rep

--- workflow/tests/test_getData.py
+++ workflow/tests/test_getData.py
@@ -12,6 +12,6 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 @pytest.mark.getData
 def test_getData():
     assert os.path.exists(os.path.join(
-        test_output_path, 'Q-Y5F6_inputBag/bagit.txt'))
+        test_output_path, '16-WJRA_inputBag/bagit.txt'))
     assert os.path.exists(os.path.join(
-        test_output_path, 'Q-Y5F6_inputBag/data/assets/Study/Q-Y4GY/Experiment/Q-Y4DP/Replicate/Q-Y5F6/mMARIS_Six2-#3.gene.rpkm.txt'))
+        test_output_path, '16-WJRA_inputBag/data/assets/Study/Q-Y4GY/Experiment/Q-Y4DP/Replicate/16-WJRA/mMARIS_Six2-#3.gene.rpkm.txt'))
--- workflow/tests/test_trimData.py
+++ workflow/tests/test_trimData.py
@@ -12,12 +12,12 @@ test_output_path = os.path.dirname(os.path.abspath(__file__)) + \
 @pytest.mark.trimData
 def test_trimData_se():
     assert os.path.exists(os.path.join(
-        test_output_path, 'Q-Y5F6_1M.se_trimmed.fq.gz'))
+        test_output_path, '16-WJRA_1M.se_trimmed.fq.gz'))
 
 
 @pytest.mark.trimData
 def test_trimData_pe():
     assert os.path.exists(os.path.join(
-        test_output_path, 'Q-Y5F6_1M.pe_val_1.fq.gz'))
+        test_output_path, '16-WJRA_1M.pe_val_1.fq.gz'))
     assert os.path.exists(os.path.join(
-        test_output_path, 'Q-Y5F6_1M.pe_val_2.fq.gz'))
+        test_output_path, '16-WJRA_1M.pe_val_2.fq.gz'))
