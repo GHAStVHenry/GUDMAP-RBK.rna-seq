600640c08304f1347873878f02febe659b9daaf3
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-12-30 00:24:48 -0600

    Give pages script

--- get-updated-badge-info.sh
+++ get-updated-badge-info.sh
@@ -6,7 +6,7 @@ current_pipeline_version=$(cat ./workflow/nextflow.config | grep -o version.* | 
 current_nextflow_version=$(cat ./workflow/nextflow.config | grep -o nextflowVersion.* | grep -oP "(?<=').*(?=')")
 
 echo "collecting badges"
-mkdir public
-curl --request GET https://img.shields.io/badge/Latest%20Release-${latest_release_tag}-green?style=plastic > ./public/release.svg
-curl --request GET https://img.shields.io/badge/Pipeline%20Version-${current_pipeline_version}-green?style=plastic > ./public/pipeline.svg
-curl --request GET https://img.shields.io/badge/Nextflow%20Version-${current_nextflow_version}-green?style=plastic > ./public/nextflow.svg
+mkdir badges
+curl --request GET https://img.shields.io/badge/Latest%20Release-${latest_release_tag}-green?style=plastic > ./badges/release.svg
+curl --request GET https://img.shields.io/badge/Pipeline%20Version-${current_pipeline_version}-green?style=plastic > ./badges/pipeline.svg
+curl --request GET https://img.shields.io/badge/Nextflow%20Version-${current_nextflow_version}-green?style=plastic > ./badges/nextflow.svg
