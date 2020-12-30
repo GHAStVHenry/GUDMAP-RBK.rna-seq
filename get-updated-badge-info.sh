16d1698eb73586fb8d1368c1673bf0139006a3c3
Gervaise H. Henry <gervaise.henry@utsouthwestern.edu>
2020-12-30 00:48:28 -0600

    Create badges for branches

--- get-updated-badge-info.sh
+++ get-updated-badge-info.sh
@@ -2,11 +2,19 @@
 
 echo "collecting stats for badges"
 latest_release_tag=$(git tag --sort=-committerdate -l *.*.* | head -1)
-current_pipeline_version=$(cat ./workflow/nextflow.config | grep -o version.* | grep -oP "(?<=').*(?=')")
-current_nextflow_version=$(cat ./workflow/nextflow.config | grep -o nextflowVersion.* | grep -oP "(?<=').*(?=')")
+current_pipeline_version=$(git show ${latest_release_tag}:workflow/nextflow.config | grep -o version.* | grep -oP "(?<=').*(?=')")
+current_nextflow_version=$(git show ${latest_release_tag}:workflow/nextflow.config | grep -o nextflowVersion.* | grep -oP "(?<=').*(?=')")
+master_pipeline_version=$(git show master:workflow/nextflow.config | grep -o version.* | grep -oP "(?<=').*(?=')")
+master_nextflow_version=$(git show master:workflow/nextflow.config | grep -o nextflowVersion.* | grep -oP "(?<=').*(?=')")
+develop_pipeline_version=$(git show develop:workflow/nextflow.config | grep -o version.* | grep -oP "(?<=').*(?=')")
+develop_nextflow_version=$(git show develop:workflow/nextflow.config | grep -o nextflowVersion.* | grep -oP "(?<=').*(?=')")
 
 echo "collecting badges"
 mkdir badges
 curl --request GET https://img.shields.io/badge/Latest%20Release-${latest_release_tag}-green?style=plastic > ./badges/release.svg
-curl --request GET https://img.shields.io/badge/Pipeline%20Version-${current_pipeline_version}-green?style=plastic > ./badges/pipeline.svg
-curl --request GET https://img.shields.io/badge/Nextflow%20Version-${current_nextflow_version}-green?style=plastic > ./badges/nextflow.svg
+curl --request GET https://img.shields.io/badge/Pipeline%20Version branch-${current_pipeline_version}-green?style=plastic > ./badges/releasePipeline.svg
+curl --request GET https://img.shields.io/badge/Nextflow%20Version-${current_nextflow_version}-green?style=plastic > ./badges/releaseNextflow.svg
+curl --request GET https://img.shields.io/badge/Pipeline%20Version branch-${current_pipeline_version}-green?style=plastic > ./badges/masterPipeline.svg
+curl --request GET https://img.shields.io/badge/Nextflow%20Version-${current_nextflow_version}-green?style=plastic > ./badges/masterNextflow.svg
+curl --request GET https://img.shields.io/badge/Pipeline%20Version branch-${current_pipeline_version}-green?style=plastic > ./badges/developPipeline.svg
+curl --request GET https://img.shields.io/badge/Nextflow%20Version-${current_nextflow_version}-green?style=plastic > ./badges/developNextflow.svg
